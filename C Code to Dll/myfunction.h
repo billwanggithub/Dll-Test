a1df566fcb2a05365cb53d107a3f19dac2301aa5
billwanggithub <billwang.github@gmail.com>
2023-11-23 19:23:03 +0800

    Add struct to header

--- C Code to Dll/myfunction.h
+++ C Code to Dll/myfunction.h
@@ -2,13 +2,27 @@
 #define MYFUNCTION_H
 
 #define DllExport   __declspec( dllexport )
+#define DLLImport __declspec(dllimport)
+
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
-DllExport int  func_add(int a, int b);
-DllExport int  func_sub(int a, int b);
+
+	struct MyData
+	{
+		/* Put data here. */
+		char* name;
+		unsigned age;
+	};
+
+	// export struct
+	DllExport struct MyData myData; 
+
+	//export function
+	DllExport int  func_add(int a, int b); 
+	DllExport int  func_sub(int a, int b);
 
 #if defined(__cplusplus)
 }
