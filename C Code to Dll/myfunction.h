f1ad707b88138279ebb3df2119ce18e0057251b1
billwanggithub <billwang.github@gmail.com>
2023-11-23 19:11:46 +0800

    Update header file

--- C Code to Dll/myfunction.h
+++ C Code to Dll/myfunction.h
@@ -1,3 +1,17 @@
+#ifndef MYFUNCTION_H
+#define MYFUNCTION_H
+
 #define DllExport   __declspec( dllexport )
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 DllExport int  func_add(int a, int b);
 DllExport int  func_sub(int a, int b);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /*guard*/
